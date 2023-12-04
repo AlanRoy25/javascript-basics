9dd56642e85c12623b597fe793c7fdd00d36978c
AlanRoy25 <alanroythomas4@gmail.com>
2023-12-04 15:30:29 +0530

    conversion of datatyoes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,5 +1,10 @@
-"usescripts"
+let number = undefined
+let newNumber = String(number)
 
+console.log(typeof number)
+
+console.log(newNumber);
+console.log(typeof newNumber)
 //datatypes : primitive 
 /*
 Number 15
@@ -8,5 +13,17 @@ boolen: true false
 null : type Object
 undefined : type undefined
 symbol : unique and many more 
-*/
 
+
+conversion:
+
+"33" => 33
+""33anbc" => NaN
+//true : 1; false => 0;
+
+for any conversion we need to use the captial letter of that conversiobn along with the var name
+
+*/
+let isloggedIn = 1
+let boolenisloggedIn = Boolean(isloggedIn)
+console.log(boolenisloggedIn);
