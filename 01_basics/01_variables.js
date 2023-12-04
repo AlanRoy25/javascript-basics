730cee464125af5e7ac89e54fb2d9bbab89d6ecd
AlanRoy25 <alanroythomas4@gmail.com>
2023-12-04 15:14:31 +0530

    variable nd datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 11445;
+let accountEmail = "alanroythomas4@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+
+accountEmail = "alskn@.com"
+accountPassword ="213244"
+
+console.log(accountEmail);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+// to declare variable we can use let and const 
+// without declaring the let and const also we can declare a variable but its not a good practise.
+
+/*
+prefer not to use var 
+because of issue in block scope and block scope
+
+*/ 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"usescripts"
+
+//datatypes : primitive 
+/*
+Number 15
+string ""
+boolen: true false
+null : type Object
+undefined : type undefined
+symbol : unique and many more 
+*/
+
