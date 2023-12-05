03944b1b2018d6d4846f6f9041ecc7ba0b93e1f8
AlanRoy25 <alanroythomas4@gmail.com>
2023-12-05 22:51:09 +0530

    Basics of JS

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,36 @@
+console.log("Operators in js");
+let a =10;
+let b =5;
+
+//arthimetic operators
+// console.log("a + b", a+b);
+// console.log("a - b", a-b);
+// console.log("a / b", a/b);
+// console.log("a * b", a*b);
+// console.log("a ** b", a**b);
+// console.log("a % b", a%b);
+// console.log("a++", a++);
+// console.log("a--", a--);
+// console.log("++a", ++a);
+// console.log("--a", --a);
+
+//assignment operators
+let ass = 4;
+ass += 5
+ass -= 5
+ass *= 5
+ass /= 5
+ass %= 5
+ass **= 5
+
+console.log("a +=", ass);
+console.log("a -=", ass);
+console.log("a *=", ass);
+console.log("a /=", ass);
+console.log("a %=", ass);
+console.log("a **=", ass);
+
+//logical operators
+
+// &&
+// ||
