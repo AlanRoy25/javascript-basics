0c3a01bedcb2d21ea800823d6224cbfecd0d5557
AlanRoy25 <alanroythomas4@gmail.com>
2023-12-08 22:49:14 +0530

    objects and json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,10 +29,35 @@ const obj2 = {4: 'o', 5: 'v'}
 
  const obj3 = { ...obj1, ...obj2} // spread type
 
-console.log(obj3)
+// console.log(obj3)
 
-console.log(Object.keys(tinderUser)) // this will give the keys and the best part is that it will convert the object to array format which we can use later for loops and other such things
-console.log(Object.values(tinderUser)) //this will give the values of the keys
-console.log(Object.entries(tinderUser)) // this  will the key value pair
+// console.log(Object.keys(tinderUser)) // this will give the keys and the best part is that it will convert the object to array format which we can use later for loops and other such things
+// console.log(Object.values(tinderUser)) //this will give the values of the keys
+// console.log(Object.entries(tinderUser)) // this  will the key value pair
 
-console.log(tinderUser.hasOwnProperty('email')) // this will check if this element is available or not.. it will give in true and false
+// console.log(tinderUser.hasOwnProperty('email')) // this will check if this element is available or not.. it will give in true and false
+
+
+const course = {
+  coursename : ' js in hindi',
+  price : '2000',
+  coursechannel: ' chai with code'
+}
+
+const {coursechannel : channel} = course // this another way of calling the object and we can change the name too according to our wish
+
+// object destructur { } - curly brackets
+console.log(channel);
+
+{
+  "name": 'Alan Roy',
+  "phNo" : '34235',
+  "emailid": "asln@gmail.com",
+  
+}
+
+{} { 
+  []
+}
+[ ] // these are the various ways of declaring the JSON : it can be an object , can also be in an array format.
+// JSONRANDOM ME : check out for the code.
